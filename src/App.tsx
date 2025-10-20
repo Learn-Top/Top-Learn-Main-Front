4ef4f5401d74b5007f2184fc63182d71bbc7fac1
AliDeWeb <AliMoradi0Business@gmail.com>
2025-10-20 23:01:25 +0330

    feat: add and config react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route } from "react-router";
+
 import "./App.css";
+import { Routes } from "react-router";
+import { Home } from "./pages/Home.page";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
