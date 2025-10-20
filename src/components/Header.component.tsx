17bc45342ef85ac95f15cbf7d6518ed1abdf6027
AliDeWeb <AliMoradi0Business@gmail.com>
2025-10-20 23:35:29 +0330

    feat: add pattern to home page

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -18,20 +18,20 @@ const HeaderMenus: IHeaderMenu[] = [
 
 export const Header = () => {
   return (
-    <div className="container flex items-center justify-between py-6">
+    <header className="container flex items-center justify-between py-6">
       <div className="flex items-center gap-10">
         <h1 className="font-Bakh text-secondary text-3xl font-extrabold">
           تاپ لرن
         </h1>
 
         <div className="hidden items-center gap-4 sm:flex">
-          {HeaderMenus.map((el) => (
-            <HeaderLink {...el} />
+          {HeaderMenus.map((el, index) => (
+            <HeaderLink key={`header-menu-${index}`} {...el} />
           ))}
         </div>
       </div>
 
       <GoToApp />
-    </div>
+    </header>
   );
 };
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "../components/Header.component";
+import { HomeTopPattern } from "../components/HomeTopPattern.component";
 
 export const Home = () => {
   return (
-    <>
+    <div className="min-h-dvh">
+      <HomeTopPattern />
+
       <Header />
-    </>
+    </div>
   );
 };
